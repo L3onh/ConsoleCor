5aa3f396bc57ca539952b49f63e3c0998843e943
L3onh <leonh.engsoftware@gmail.com>
2021-01-09 16:20:32 -0300

    First Commit

---
+++ GameTOP.Lib/Jogador3.cs
@@ -0,0 +1,20 @@
+namespace GameTOP.Lib
+{
+   public class Jogador3 : iJogador
+   {
+      public string Chuta()
+      {
+         return "teste Chuta \n";
+      }
+
+      public string Corre()
+      {
+         return "testeCorre \n";
+      }
+
+      public string Passe()
+      {
+         return "teste Passe \n";
+      }
+   }
+}
